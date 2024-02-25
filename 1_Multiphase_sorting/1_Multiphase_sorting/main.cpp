41a3e08caac745191bb29b80bc0fcce360e988bc
User <User@DESKTOP-RE2P47L.local>
2024-02-25 22:34:11 +0700

    Добавлена функция создания потока ofstream(пустого файла на запись) и функция создающая vector<ofstream>

--- 1_Multiphase_sorting/1_Multiphase_sorting/main.cpp
+++ 1_Multiphase_sorting/1_Multiphase_sorting/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
 
 bool isFileContainsSortedArray(const std::string& fileName)
 {
@@ -25,12 +26,31 @@ bool isFileContainsSortedArray(const std::string& fileName)
 	return true;
 }
 
-void MultiphaseSort(std::string &fileName, const int countFiles = 3)
-{
-
+std::ofstream createOfstream(const std::string& fileName) {
+	std::ofstream stream(fileName);
+	return stream;
 }
+
+std::vector<std::ofstream> CreateFilesVector(const int countFiles)
+{
+	std::vector<std::ofstream> filesContainer;
+	for (int i = 0; i < countFiles; ++i)
+	{
+		std::string fileName = "file" + std::to_string(i) + ".txt";
+		filesContainer.push_back(createOfstream(fileName));
+	}
+	return filesContainer;
+}
+
+//void MultiphaseSort(std::string &fileName, const int countFiles = 3)
+//{
+//	std::fstream** files = new std::fstream * [countFiles];
+//	for (int i = 0; i < countFiles; ++i)
+//		files[i] = new std::fstream();
+//}
 
 int main()
 {
-
+	std::vector<std::ofstream> files = CreateFilesVector(3);
+	files[0] << 52;
 }
