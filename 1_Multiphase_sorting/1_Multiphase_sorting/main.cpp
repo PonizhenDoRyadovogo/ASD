970e598ddd204de66356bac572a00e4d7ff4490c
User <User@DESKTOP-RE2P47L.local>
2024-02-29 10:54:39 +0700

    изменил ofstream на fstream, начал этап слияния

--- 1_Multiphase_sorting/1_Multiphase_sorting/main.cpp
+++ 1_Multiphase_sorting/1_Multiphase_sorting/main.cpp
@@ -26,26 +26,28 @@ bool isFileContainsSortedArray(const std::string& fileName)
 	return true;
 }
 
-std::ofstream createOfstream(const std::string& fileName) {
-	std::ofstream stream(fileName);
+std::fstream createOfstream(const std::string& fileName) {
+	std::fstream stream(fileName, std::ios::out);
 	return stream;
 }
 
-std::vector<std::ofstream> CreateFilesVector(const int countFiles)
+std::vector<std::fstream> CreateFilesVector(const int countFiles, std::string *names)
 {
-	std::vector<std::ofstream> filesContainer;
+	std::vector<std::fstream> filesContainer;
 	for (int i = 0; i < countFiles; ++i)
 	{
 		std::string fileName = "file" + std::to_string(i) + ".txt";
+		names[i] = fileName;
 		filesContainer.push_back(createOfstream(fileName));
 	}
 	return filesContainer;
 }
 
-void MultiphaseSort(std::string &fileName, const int countFiles = 3)
+void MultiphaseSort(std::string& fileName, const int countFiles = 3)
 {
 	//1
-	std::vector<std::ofstream> files = CreateFilesVector(countFiles);
+	std::string* namesFiles = new std::string[countFiles];
+	std::vector<std::fstream> files = CreateFilesVector(countFiles, namesFiles);
 	std::ifstream originFile(fileName);
 	if (!originFile.is_open())
 	{
@@ -54,36 +56,41 @@ void MultiphaseSort(std::string &fileName, const int countFiles = 3)
 	}
 	int* ms = new int[countFiles];
 	int* ip = new int[countFiles];
-	for (int i = 0; i < countFiles; ++i)
+	for (int i = 0; i <= countFiles - 2; ++i)
 	{
 		ip[i] = 1;
 		ms[i] = 1;
-		if (i == countFiles - 1)
-		{
-			ip[i] = 0;
-			ms[i] = 0;
-		}
 	}
-	int level = 1, it = 0, actual, next;
+	ip[countFiles - 1] = ms[countFiles - 1] = 0;
+	int level = 1, it = 0, actual, next, countSeparator = 0;
 	//2
 	originFile >> actual;
 	while (!originFile.eof())
 	{
-		files[it] << actual << " ";
+		if (actual != INT_MAX)
+		{
+			files[it] << actual << " ";
+		}
+		else
+			countSeparator += 1;
 		originFile >> next;
 		while (!originFile.eof() && (actual <= next))
 		{
 			actual = next;
-			files[it] << actual << " ";
+			if (actual != INT_MAX)
+				files[it] << actual << " ";
+			else
+				countSeparator += 1;
 			originFile >> next;
 		}
 		ms[it]--;
-	//3
+		files[it] << INT_MAX << " ";
+		//3
 		if (ms[it] < ms[it + 1])
 			++it;
 		else if (ms[it] == 0)
 		{
-	//4
+			//4
 			++level;
 			int ip0 = ip[0];
 			it = 0;
@@ -96,14 +103,56 @@ void MultiphaseSort(std::string &fileName, const int countFiles = 3)
 		else
 			it = 0;
 		actual = next;
-		if (originFile.eof())
-			files[it] << actual;
+		if (originFile.eof() && actual != INT_MAX)
+			files[it] << actual<< " " << INT_MAX;
 	}
+	//2 end
+	for (int i = 0; i < countFiles; ++i)
+	{
+		files[i].close();
+	}
+	originFile.close();
 
+	//5
+	for (int i = 0; i <= countFiles - 2; ++i)
+	{
+		files[i].open(namesFiles[i], std::ios::in);
+	}
+	files[countFiles - 1].open(namesFiles[countFiles - 1], std::ios::out);
+	//while (level > 0)
+	//{
+	//	while (!files[countFiles - 2].eof())
+	//	{
+	//		int haveFictitious = 1;
+	//		for (int i = 0; i <= countFiles - 2; ++i)
+	//			haveFictitious *= ms[i];
+	//		while (haveFictitious)
+	//		{
+	//			for (int i = 0; i <= countFiles - 2; ++i)
+	//			{
+	//				--ms[i];
+	//				haveFictitious *= ms[i];
+	//			}
+	//			++ms[countFiles - 1];
+	//		}
+	////7.2
+	//		for (int i = 0; i < countFiles; ++i)
+	//		{
+	//			if (ms[i] == 0)
+	//			{
+
+	//			}
+	//			else if(ms[i] > 0)
+	//				--ms[i];
+	//		}
+	//	}
+	//}
+
+	delete[]namesFiles;
 }
 
 int main()
 {
 	std::string file = "originFile.txt";
-	MultiphaseSort(file, 4);
+	MultiphaseSort(file, 5);
 }
