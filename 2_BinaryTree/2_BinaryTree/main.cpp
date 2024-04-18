c53af30e7be955d529719f2dc515a2562eebb31f
User <User@DESKTOP-RE2P47L.local>
2024-04-18 15:27:05 +0700

    Изменение в main. Удалил код для QT

--- 2_BinaryTree/2_BinaryTree/main.cpp
+++ 2_BinaryTree/2_BinaryTree/main.cpp
@@ -52,21 +52,4 @@
 //    return 0;
 //}
 
-#include <stdlib.h>
 
-#include <QApplication>
-#include <QDebug>
-#include<QGraphicsPixmapItem>
-#include<QGraphicsView>
-#include "TreeWidget.h"
-
-int main(int argc, char *argv[])
-{
-    QApplication app(argc, argv);
-    TreeWidget widget;
-    widget.show();
-    for (int i = 0; i < 20; ++i) {
-        widget.addKey(rand() % 100);
-    }
-    return app.exec();
-}
