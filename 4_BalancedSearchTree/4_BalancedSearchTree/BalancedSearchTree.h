598cc27ed7a918cfcf3e570650ff1b0b29697138
User <User@DESKTOP-RE2P47L.local>
2024-04-25 13:48:58 +0700

    Создан проект оптимального дерева поиска

---
+++ 4_BalancedSearchTree/4_BalancedSearchTree/BalancedSearchTree.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include "../../3_BinaryTreeSearch/3_BinaryTreeSearch/BinaryTreeSearch.h"
+
+class BalancedSearchTree : public BinaryTreeSearch
+{
+	BalancedSearchTree() = default;
+	~BalancedSearchTree() override = default;
+	
+};
+
