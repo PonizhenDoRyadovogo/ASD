b2294577047756e94c49a3c45d57e31e11fd5f08
User <User@DESKTOP-RE2P47L.local>
2024-05-11 17:21:07 +0700

    Создан проект

---
+++ 5_HuffmanTree/5_HuffmanTree/HuffmanTree.h
@@ -0,0 +1,21 @@
+#pragma once
+
+//#include "C:\STUDY\Repo\932221b\ThirdLab OOP\ThirdLab OOP"
+class HuffmanTree
+{
+public:
+	class Node;
+public:
+
+private:
+	Node* m_root = nullptr;
+};
+
+class HuffmanTree::Node
+{
+public:
+private:
+	Node* m_left = nullptr;
+	Node* m_right = nullptr;
+
+};
